4a67f46d0202519f4d9a2e0a8147d28518cd818d
John Wood <johnpwoodiv@gmail.com>
2017-12-01 21:32:18 -0600

    implement Assembler.rb without symbol replacement

---
+++ projects/06/Assembler.rb
@@ -0,0 +1,50 @@
+arr= IO.readlines ARGV[0]
+
+#remove comments
+arr.each do |x|
+  x.gsub!(/\/\/.*/, "")
+  x.strip!
+end
+arr.select! {|x| x != ""}
+
+#replace symbols (TO-DO)
+
+#assemble
+jmp = ["", "jgt", "jeq", "jge", "jlt", "jne", "jle", "jmp"]
+
+comp = {  "0"=>"101010",   "1"=>"111111", "-1"  =>"111010",
+          "d"=>"001100",   "a"=>"110000", "!d"  =>"001101",
+         "!a"=>"110001", "-d" =>"001111", "-a"  =>"110011",
+        "d+1"=>"011111", "a+1"=>"110111", "d-1" =>"001110",
+        "a-1"=>"110010", "d+a"=>"000010", "d-a" =>"010011",
+        "a-d"=>"000111", "d&a"=>"000000", "d|a" =>"010101"}        
+
+arr.each do |x|
+  x.replace(x.downcase)
+  # A-type instructions:
+  if x[0] == "@"
+    x.replace(sprintf("0%015b", x[1..-1].to_i))
+  else
+  # C-type instructions:
+    x =~ /(\w*)=?([^;]+);?(\w*)/
+    d = $1
+    c = $2
+    j = $3
+
+    dest = 0
+    dest += 4 if d.include? "a"
+    dest += 2 if d.include? "d"
+    dest += 1 if d.include? "m"
+    
+    a = c.include?("m")? "1": "0"
+
+    c.gsub!("m", "a")
+    
+    x.replace(sprintf("111%03b|%s|%s|%03b", dest, a, comp[c], jmp.index(j)))
+    x.replace(sprintf("111%s%s%03b%03b", a, comp[c], dest, jmp.index(j)))
+  end
+end
+
+arr.each do |x|
+  puts x
+end
